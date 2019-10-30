c6faa18cabb74724f424322690380ade78207db2
a944456435 <944456435@qq.com>
2019-10-30 19:34:41 +0800

    文件上传之前的修改

---
+++ src/pages/category/categoryDetail.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-row>
+      <el-col><h2>订单详情</h2> </el-col>
+      <el-col :offset="20"><el-button type="text" @click="backHandler">返回</el-button></el-col>
+    </el-row>
+    <div>栏目名称：{{ category.name }}</div>
+    <div>栏目数量：{{ category.num }}</div>
+    <div>父级栏目：{{ category.parentId }}</div>
+    <div>栏目主图：{{ category.photo }}</div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      category: []
+    }
+  },
+  created() {
+    this.category = this.$route.query.category
+  },
+  methods: {
+    backHandler() {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
